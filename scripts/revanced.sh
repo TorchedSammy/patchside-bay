9f0e93cc0130b33a9c4a12c03ffb74f84ea37af1
sammyette <torchedsammy@gmail.com>
2023-02-21 21:13:03 -0400

    ci: setup actions for patched youtube

---
+++ scripts/revanced.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl -s https://api.github.com/repos/revanced/revanced-cli/releases/latest | grep ".jar" | cut -d : -f 2,3 | tr -d \" | awk 'NR>1' | awk '{$1=$1;print}' | xargs curl -L -o revanced-cli.jar
+curl -s https://api.github.com/repos/revanced/revanced-patches/releases/latest | grep ".jar" | cut -d : -f 2,3 | tr -d \" | awk 'NR>1' | awk '{$1=$1;print}' | xargs curl -L -o revanced-patches.jar
